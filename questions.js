ff2d8d870f3cbc82eba1206f7ba31b2a83435fcf
Lateef Amoo OLawale <lateefamoo57@gmail.com>
2026-03-14 00:04:45 +0100

    Add quiz questions to questions.js

--- questions.js
+++ questions.js
@@ -1,1 +1,24 @@
+const quizQuestions = [
 
+{
+question: "What is 5 + 3 ?",
+options: ["6","7","8","9"],
+correct: 2,
+solution: "5 + 3 = 8"
+},
+
+{
+question: "Which is a vowel?",
+options: ["B","D","A","F"],
+correct: 2,
+solution: "A is a vowel"
+},
+
+{
+question: "What is 10 - 4 ?",
+options: ["5","6","7","8"],
+correct: 1,
+solution: "10 - 4 = 6"
+}
+
+];
