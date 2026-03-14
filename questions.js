3ed62892a7f8fcddf3ea2ea7a1c4ffe4b7827205
Lateef Amoo OLawale <lateefamoo57@gmail.com>
2026-03-14 17:42:19 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,7 +1,7 @@
 const quizQuestions = [
 
 {
-question: "What is 5 + 3 ?",
+question: "What is 5 + 3?",
 options: ["6","7","8","9"],
 correct: 2,
 solution: "5 + 3 = 8"
@@ -15,7 +15,7 @@ solution: "A is a vowel"
 },
 
 {
-question: "What is 10 - 4 ?",
+question: "What is 10 - 4?",
 options: ["5","6","7","8"],
 correct: 1,
 solution: "10 - 4 = 6"
@@ -26,6 +26,23 @@ question: "What is the name of the Fountain of Achiever’s school proprietor?",
 options: ["Mr Akinfolarin","Mr Pastor","Mrs Akinfolarin"],
 correct: 0,
 solution: "The proprietor is Mr Akinfolarin."
+},
+
+{
+question: "What is the product of 15 and 2?",
+options: ["25","152","30"],
+correct: 2,
+solution: "15 × 2 = 30"
+},
+
+{
+question: "What is the name of the Fountain of Achiever’s school proprietress?",
+options: ["Mr Akinfolarin","Mr Pastor","Mrs Akinfolarin"],
+correct: 2,
+solution: "The proprietress is Mrs Akinfolarin."
+}
+
+];
 },
 
 {
