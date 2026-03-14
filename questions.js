55ccb72d7d79ae6a13c9e65377ce1c02f62bedc7
Lateef Amoo OLawale <lateefamoo57@gmail.com>
2026-03-14 17:26:06 +0100

    Add quiz questions to questions.js

--- questions.js
+++ questions.js
@@ -22,3 +22,27 @@ solution: "10 - 4 = 6"
 }
 
 ];
+const quizQuestions = [
+
+{
+question: "What is the name of the Fountain of Achiever’s school proprietor?",
+options: ["Mr Akinfolarin","Mr Pastor","Mrs Akinfolarin"],
+correct: 0,
+solution: "The proprietor is Mr Akinfolarin."
+},
+
+{
+question: "What is the product of 15 and 2?",
+options: ["25","152","30"],
+correct: 2,
+solution: "15 × 2 = 30"
+},
+
+{
+question: "What is the name of the Fountain of Achiever’s school proprietress?",
+options: ["Mr Akinfolarin","Mr Pastor","Mrs Akinfolarin"],
+correct: 2,
+solution: "The proprietress is Mrs Akinfolarin."
+}
+
+];
